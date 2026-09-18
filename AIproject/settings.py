c3604d86164b4a904fbd7df3b876ddc7ffebdf65
Joe-Takayama <jgaoshan549@gmail.com>
2026-09-18 13:18:52 +0900

    アカウント用のファイルを追加

--- AIproject/settings.py
+++ AIproject/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'AIapp.apps.AiappConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
--- AIproject/urls.py
+++ AIproject/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('AI_Chat/', include('AIapp.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
